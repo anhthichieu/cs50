826c915fb3c53004f03c46d6ae1aa75efe9f19a2
anhthichieu <62415015+anhthichieu@users.noreply.github.com>
2020-06-15 17:28:25 +0700

    Update substitution.c

--- pset2/substitution.c
+++ pset2/substitution.c
@@ -65,12 +65,12 @@ int main(int argc, string argv[])
             // Letter mapping & preserve case
             if (isupper(plainText[i]))
             {
-                printIndex = plainText[i] - 65;
+                printIndex = plainText[i] - 65; // From https://asciichart.com/
                 argv[1][printIndex] = toupper(argv[1][printIndex]);
             }
             else if (islower(plainText[i]))
             {
-                printIndex = plainText[i] - 97;
+                printIndex = plainText[i] - 97; // From https://asciichart.com/
                 argv[1][printIndex] = tolower(argv[1][printIndex]);
             }
             printf("%c", argv[1][printIndex]);
